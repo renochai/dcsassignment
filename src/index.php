2e5cad74766f171af67a7098b293438b26e64fd5
renochai <renocjh@gmail.com>
2017-04-04 03:40:21 +0800

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,11 +1,128 @@
-<?php
+<style type="text/css">
+a.btnLnk {float:right; display:block; height:50px; width:150px; background:#2e3192; padding-top:15px; text-align:center; color:#fff; font-size:16px; font-weight:300; box-sizing:border-box;}
 
-echo "<table style=\"margin:auto;\">
-	<tbody>
-		<tr>
-			<td>
-				Hello World
-			</td>
-		</tr>
-	<tbody>
-</table>";
+a.btnLnk:link {text-decoration:none;}
+a.btnLnk:visited {text-decoration:none;}
+a.btnLnk:hover {text-decoration:none; background:#3e41a9;}
+a.btnLnk:active {text-decoration:none; background:#3e41a9;}
+
+</style>
+<html>
+	<head>
+		<title>Docker R&D Team</title>
+	</head>
+	<body>
+		<table style="margin:auto; width:960px; background-color: ">
+			<tbody>
+				<tr style="background-color:gray">
+					<td>
+						<h1>Greetings from the Docker Research Team</h1>
+						<ul style="line-height:30px">
+							Team Member:
+							<li>
+								Chai Jun Hui</li>
+							<li>
+								Tan Ming Xuan</li>
+							<li>
+								Dylan</li>
+						</ul>
+					</td>
+				</tr>
+				<tr><td style="height:30px"></td></tr>
+				<tr>
+					<td>
+						<table style="border:1px solid black" width="100%">
+							<tbody>
+								<tr>
+									<td>
+										<h2>Useful Link for Our Research:</h2>
+									</td>
+								</tr>
+								<tr style="background-color:gray">
+									<td>
+										<table width="100%">
+											<tbody>
+												<tr>
+													<td style="width:205px">
+														<h3>Dockers Official Website:</h3>
+													</td>
+													<td style="width:20px"></td>
+													<td><a target="blank" class="btnLnk" href="https://www.docker.com/" title="Click to learn more about Docker!">Link</a></td>
+												</tr>
+											</tbody>
+										</table>
+									</td>
+								</tr>
+								<tr><td></td></tr>
+								<tr style="background-color:gray">
+									<td>
+										<table width="100%">
+											<tbody>
+												<tr>
+													<td style="width:205px">
+														<h3>Dockers Repository:</h3>
+													</td>
+													<td style="width:20px"></td>
+													<td><a target="blank" class="btnLnk" href="https://hub.docker.com/" title="View your pushed Docker images here!">Link</a></td>
+												</tr>
+											</tbody>
+										</table>
+									</td>
+								</tr>
+								<tr><td></td></tr>
+								<tr style="background-color:gray">
+									<td>
+										<table width="100%">
+											<tbody>
+												<tr>
+													<td style="width:205px">
+														<h3>Google Cloud Platform:</h3>
+													</td>
+													<td style="width:20px"></td>
+													<td><a target="blank" class="btnLnk" href="https://cloud.google.com" title="Click to Learn more about Google Cloud Platform!">Link</a></td>
+												</tr>
+											</tbody>
+										</table>
+									</td>
+								</tr>
+								<tr><td></td></tr>
+								<tr style="background-color:gray">
+									<td>
+										<table width="100%">
+											<tbody>
+												<tr>
+													<td style="width:205px">
+														<h3>GitHub:</h3>
+													</td>
+													<td style="width:20px"></td>
+													<td><a target="blank" class="btnLnk" href="https://github.com/" title="Click to Learn more about GitHub!">Link</a></td>
+												</tr>
+											</tbody>
+										</table>
+									</td>
+								</tr>
+								<tr><td></td></tr>
+								<tr style="background-color:gray">
+									<td>
+										<table width="100%">
+											<tbody>
+												<tr>
+													<td style="width:205px">
+														<h3>YouTube: </h3>
+													</td>
+													<td style="width:20px"></td>
+													<td><a target="blank" class="btnLnk" href="https://www.youtube.com/watch?v=YFl2mCHdv24&t=423s&index=6&list=PLlj9BrHKq9WKaz8UV3BjEqicn-C3qHxy4" title="Watch and Learn!">Link</a></td>
+												</tr>
+											</tbody>
+										</table>
+									</td>
+								</tr>
+								<tr><td></td></tr>
+							</tbody>
+						</table>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</body>
+</html>
